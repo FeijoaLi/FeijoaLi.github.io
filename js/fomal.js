726d60c3ba91cc190acce2efbebe7e1319b60fc8
FeijoaLi <1051553593@qq.com>
2025-08-01 08:42:18 +0800

    Site updated: 2025-08-01 08:42:17

--- js/fomal.js
+++ js/fomal.js
@@ -2855,9 +2855,9 @@ if (window.localStorage.getItem("fpson") == undefined || window.localStorage.get
       } else if (fps < 35) {
         var kd = `<span style="color:#9338e6">不太流畅🙄</span>`
       } else if (fps <= 45) {
-        var kd = `<span style="color:#08b7e4">还不错哦😁</span>`
+        var kd = `<span style="color:#08b7e4">还不错哦🤣</span>`
       } else {
-        var kd = `<span style="color:#39c5bb">十分流畅🤣</span>`
+        var kd = `<span style="color:#39c5bb">十分流畅😁</span>`
       }
       document.getElementById("fps").innerHTML = `FPS:${fps} ${kd}`;
       frame = 0;
